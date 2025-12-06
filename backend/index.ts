c2e07c5c26f8a2634b65307a0e62c766bf384f7e
Pak Yuri <arjuna7181@gmail.com>
2025-12-06 21:02:54 +0700

    Setup express

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express  from "express";
+import {config} from "dotenv";
+
+config();
+const app = express();
+const port = process.env.PORT
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use("/api/auth", authRoutes);
+app.listen(port, () => {
+    console.log(`Server is running ]at http://localhost:${port}`);
+});
