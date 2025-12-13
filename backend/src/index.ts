1d6ca19143d9aebc20586955c967223e289483b3
Pak Yuri <arjuna7181@gmail.com>
2025-12-14 02:20:20 +0700

    xxx

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express  from "express";
 import {config} from "dotenv";
 import authRoutes from "./routes/auth.route.ts";
 
+
 config();
 const app = express();
 const port = process.env.PORT
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
 app.listen(port, () => {
     console.log(`Server is running ]at http://localhost:${port}`);
 });
---
+++ backend/src/routes/dashboard.route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { totalPerawat, totalPenunjangMedis, totalUmum, totalDinas, shiftPagi, shiftSore, shiftMalam } from '../controller/dashboard.controller.ts';
+
+const router = express.Router();
+router.get('/total-perawat', totalPerawat);
+router.get('/total-penunjang-medis', totalPenunjangMedis);
+router.get('/total-umum', totalUmum);
+router.get('/total-dinas', totalDinas);
+router.get('/shift-pagi', shiftPagi);
+router.get('/shift-sore', shiftSore);
+router.get('/shift-malam', shiftMalam);
+
+export default router;
