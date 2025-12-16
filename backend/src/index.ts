51cd80fda95327ff60671c83b5c02ffd233c458f
Putraaa <61768347+vickycxc@users.noreply.github.com>
2025-12-17 01:51:08 +0700

    halo3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import express  from "express";
-import {config} from "dotenv";
+import express from "express";
+import { config } from "dotenv";
 import authRoutes from "./routes/auth.route.ts";
 import staffRoutes from "./routes/staff.route.ts";
 import kinerjastaffRoutes from "./routes/kinerjastaff.route.ts";
@@ -7,20 +7,15 @@ import cors from "cors";
 
 config();
 const app = express();
-const port = process.env.PORT
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+const port = process.env.PORT;
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/staff", staffRoutes);
 app.use("/api/kinerjaStaff", kinerjastaffRoutes);
 // app.use("/api/direktori", direktoriRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running ]at http://localhost:${port}`);
+  console.log(`Server is running ]at http://localhost:${port}`);
 });
