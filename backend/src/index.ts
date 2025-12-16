e0e9386ba622876544197412476a5a4b374bf287
Putraaa <61768347+vickycxc@users.noreply.github.com>
2025-12-17 01:32:07 +0700

    init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,17 @@ import {config} from "dotenv";
 import authRoutes from "./routes/auth.route.ts";
 import staffRoutes from "./routes/staff.route.ts";
 import kinerjastaffRoutes from "./routes/kinerjastaff.route.ts";
+import cors from "cors";
 
 config();
 const app = express();
 const port = process.env.PORT
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/api/auth", authRoutes);
